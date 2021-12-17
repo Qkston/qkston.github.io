cea9258da31511d4587ff1a24c88b720754ff918
Andrey Slynko <qkston22@gmail.com>
2021-12-17 16:54:42 +0200

    Fix a jump to the successful registration page

--- makets/Registration flow/script.js
+++ makets/Registration flow/script.js
@@ -4,7 +4,12 @@ const labelForIncorrectPasswords = document.querySelector(".incorrect-passwords"
 const passwordInput = document.querySelector(".password");
 const confirmPasswordInput = document.querySelector(".confirm-password");
 
+const requiredInputs = document.querySelectorAll(".required-input");
+const errorMessages = document.querySelectorAll(".main-form-label-error");
+
 const registerBtn = document.querySelector(".register-button");
+
+console.log(Array.from(requiredInputs));
 
 formOptionalBtn.forEach((element) => {
 	element.addEventListener("click", (e) => {
@@ -20,7 +25,10 @@ formOptionalBtn.forEach((element) => {
 
 confirmPasswordInput.addEventListener("keyup", () => {
 	if (passwordInput.value !== confirmPasswordInput.value) {
-		labelForIncorrectPasswords.style.display = "inline";
+		errorMessages.forEach((element) => {
+			element.style.display = "none";
+		});
+		labelForIncorrectPasswords.style.display = "inline-block";
 	} else {
 		labelForIncorrectPasswords.style.display = "none";
 	}
@@ -29,5 +37,15 @@ confirmPasswordInput.addEventListener("keyup", () => {
 registerBtn.addEventListener("click", (e) => {
 	e.preventDefault();
 
-	window.location = "https://qkston.github.io/makets/Registration%20flow/success-register.html";
+	const isEmpty = Array.from(requiredInputs).some((element) => element.value === "");
+
+	requiredInputs.forEach(() => {
+		if (isEmpty) {
+			errorMessages.forEach((element) => {
+				element.style.display = "inline-block";
+			});
+		} else {
+			location.href = "https://qkston.github.io/makets/Registration%20flow/success-register.html";
+		}
+	});
 });
