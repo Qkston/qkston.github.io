abdb5d14ec3d9c72451b91b7a29bc1a1bec684aa
Andrey Slynko <qkston22@gmail.com>
2021-12-19 14:48:15 +0200

    animation for timer is done

--- makets/Registration flow/success-register-timer.js
+++ makets/Registration flow/success-register-timer.js
@@ -1,6 +1,8 @@
 (() => {
-	const timerNumber = document.querySelector(".success-page-main-email-timer-number");
 	const timerBlock = document.querySelector(".success-page-main-email-timer");
+	const timerNumber = document.querySelector(".success-page-main-email-timer-number");
+	const timerDoneLine = document.querySelector(".success-page-main-email-timer-done");
+
 	const resendLink = document.querySelector(".success-page-main-email-resend");
 
 	window.addEventListener("load", () => {
@@ -8,6 +10,7 @@
 
 		setInterval(() => {
 			if (timer === 0) {
+				timerDoneLine.style.borderTop = "2px solid transparent";
 				timerBlock.style.display = "none";
 				resendLink.style.display = "block";
 			} else {
@@ -16,6 +19,16 @@
                ${timer}s
             `;
 				timer--;
+
+				if (30 > timer && timer >= 20) {
+					timerDoneLine.style.borderLeft = "2px solid transparent";
+				}
+				if (20 > timer && timer >= 10) {
+					timerDoneLine.style.borderBottom = "2px solid transparent";
+				}
+				if (10 > timer && timer >= 5) {
+					timerDoneLine.style.borderRight = "2px solid transparent";
+				}
 			}
 		}, 1000);
 	});
