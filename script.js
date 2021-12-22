d995301af2de11e85e5620446bdc778bc8a11e17
Andrey Slynko <qkston22@gmail.com>
2021-12-22 13:56:49 +0200

    done required inputs with js

---
+++ script.js
@@ -0,0 +1,32 @@
+const modalWindow = document.querySelector(".modal-window");
+
+const formInputs = document.querySelectorAll(".form-inputs");
+const formInputsArr = Array.from(formInputs);
+
+const sendFormBtn = document.getElementById("send-form-button");
+
+sendFormBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	const requiredInputs = formInputsArr.some((element) => element.value === "");
+
+	if (requiredInputs) {
+		console.log("bad");
+
+		formInputsArr.forEach((element) => {
+			if (element.value === "") {
+				element.style.border = "1px solid red";
+			}
+		});
+
+		setTimeout(() => {
+			formInputsArr.forEach((element) => {
+				if (element.value === "") {
+					element.style.border = "none";
+				}
+			});
+		}, 3000);
+	} else {
+		console.log("good");
+		modalWindow.style.display = "block";
+	}
+});
