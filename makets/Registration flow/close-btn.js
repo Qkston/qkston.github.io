50913e6065183e561b8dfa4e8b497cb3c2366085
Andrey Slynko <qkston22@gmail.com>
2021-12-29 21:03:34 +0200

    added links to new pages

---
+++ makets/Registration flow/close-btn.js
@@ -0,0 +1,7 @@
+const closeBtn = document.querySelector(".success-page-main-close-button");
+
+closeBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+
+	location.href = "https://qkston.github.io/makets/Registration%20flow/log-in.html";
+});
