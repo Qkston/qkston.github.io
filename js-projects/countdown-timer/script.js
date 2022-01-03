0cb157f99b91e340338cd09ad58938e9694f8ee5
Andrey Slynko <qkston22@gmail.com>
2022-01-03 14:41:15 +0200

    created a function for lowering seconds

--- js-projects/countdown-timer/script.js
+++ js-projects/countdown-timer/script.js
@@ -10,15 +10,29 @@ const hoursCountNumber = document.querySelector(".numbers-hours-count");
 const minutesCountNumber = document.querySelector(".numbers-minutes-count");
 const secondsCountNumber = document.querySelector(".numbers-seconds-count");
 
+const startBtn = document.querySelector(".start-timer");
+
 let hoursCount = 0,
 	minutesCount = 0,
 	secondsCount = 0;
 
+// Button
+// startBtn.addEventListener("click", (e) => {
+// 	e.preventDefault();
+// 	console.log("Started");
+
+// 	while (secondsCount > 0) {
+// 		setTimeout(() => {
+// 			secondsCount--;
+// 			console.log(secondsCount);
+// 		}, 1000);
+// 		// secondsDown(secondsCount);
+// 	}
+// });
+
 // Hours
 upperArrowHours.addEventListener("click", () => {
 	hoursCount++;
-
-	console.log(hoursCount);
 
 	if (hoursCount < 10) {
 		hoursCountNumber.innerHTML = `0${hoursCount}`;
@@ -52,7 +66,7 @@ upperArrowMinutes.addEventListener("click", () => {
 	} else if (minutesCount >= 10 && minutesCount < 60) {
 		minutesCountNumber.innerHTML = `${minutesCount}`;
 	} else if (minutesCount === 60) {
-		hoursCount++;
+		// hoursCount++;
 		minutesCount = 0;
 		minutesCountNumber.innerHTML = `00`;
 	}
@@ -80,21 +94,27 @@ upperArrowSeconds.addEventListener("click", () => {
 	} else if (secondsCount >= 10 && secondsCount < 60) {
 		secondsCountNumber.innerHTML = `${secondsCount}`;
 	} else if (secondsCount === 60) {
-		minutesCount++;
+		// minutesCount++;
 		secondsCount = 0;
 		secondsCountNumber.innerHTML = `00`;
 	}
 });
 
 lowerArrowSeconds.addEventListener("click", () => {
-	secondsCount--;
-
-	if (secondsCount >= 0 && secondsCount < 10) {
-		secondsCountNumber.innerHTML = `0${secondsCount}`;
-	} else if (secondsCount >= 10) {
-		secondsCountNumber.innerHTML = `${secondsCount}`;
-	} else if (secondsCount <= 0) {
-		secondsCountNumber.innerHTML = `00`;
-		secondsCount = 0;
-	}
+	secondsCount = secondsDown(secondsCount);
 });
+
+function secondsDown(count) {
+	count--;
+
+	if (count >= 0 && count < 10) {
+		secondsCountNumber.innerHTML = `0${count}`;
+	} else if (count >= 10) {
+		secondsCountNumber.innerHTML = `${count}`;
+	} else if (count <= 0) {
+		secondsCountNumber.innerHTML = `00`;
+		count = 0;
+	}
+
+	return count;
+}
