153bf9f4ed31dce2159e3cbd41938c030f3a4bf5
Andrey Slynko <qkston22@gmail.com>
2022-01-17 14:30:19 +0200

    function for show last entered character is ready

--- js-projects/character-counter/script.js
+++ js-projects/character-counter/script.js
@@ -1,11 +1,15 @@
 const header = document.querySelector(".header"),
 	countNumber = header.querySelector(".count-number"),
+	lastEnteredCharacter = header.querySelector(".character"),
 	copyBtn = header.querySelector(".btn-copy"),
 	copyMsg = header.querySelector(".msg-copy"),
 	textarea = document.querySelector(".textarea");
 
 textarea.oninput = function (event) {
 	countNumber.innerHTML = event.target.value.length;
+
+	getLastCharacter(textarea, lastEnteredCharacter);
+	// console.dir(textarea);
 };
 
 copyBtn.addEventListener("click", () => {
@@ -37,5 +41,16 @@ function checkTextareaLength(textarea, message) {
 		message.innerHTML = `Successful copied!`;
 		message.classList.add("successful");
 		message.classList.remove("error");
+	}
+}
+
+function getLastCharacter(textarea, element) {
+	let textareaArr = textarea.value.split("");
+	if (textareaArr.length == 0) {
+		element.innerHTML = "";
+	} else if (textareaArr[textareaArr.length - 1] == " ") {
+		element.innerHTML = "space";
+	} else {
+		element.innerHTML = textareaArr[textareaArr.length - 1];
 	}
 }
