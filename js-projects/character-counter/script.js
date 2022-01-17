15fee1dfefca23f8b1b20f670927d8fe98053da1
Andrey Slynko <qkston22@gmail.com>
2022-01-17 14:05:18 +0200

    function for copy text is ready (*character counter)

--- js-projects/character-counter/script.js
+++ js-projects/character-counter/script.js
@@ -1,7 +1,40 @@
 const count = document.querySelector(".count-number"),
+	msg = document.querySelector(".msg-copy"),
 	btn = document.querySelector(".btn-copy"),
 	textarea = document.querySelector(".textarea");
 
 textarea.oninput = function (event) {
 	count.innerHTML = event.target.value.length;
 };
+
+btn.addEventListener("click", () => {
+	textarea.focus();
+	textarea.select();
+	document.execCommand("copy");
+
+	checkTextareaLength(textarea, msg);
+});
+
+function checkTextareaLength(textarea, message) {
+	if (textarea.value.length <= 0) {
+		setTimeout(() => {
+			message.innerHTML = `To copy text, click this button`;
+			message.classList.remove("error");
+			message.classList.remove("successful");
+		}, 5000);
+
+		message.innerHTML = `Insert text!`;
+		message.classList.add("error");
+		message.classList.remove("successful");
+	} else {
+		setTimeout(() => {
+			message.innerHTML = `To copy text, click this button`;
+			message.classList.remove("error");
+			message.classList.remove("successful");
+		}, 5000);
+
+		message.innerHTML = `Successful copied!`;
+		message.classList.add("successful");
+		message.classList.remove("error");
+	}
+}
