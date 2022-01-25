1916aeade2ab65f80031a52112ad75d46820fa85
Andrey Slynko <qkston22@gmail.com>
2022-01-25 21:26:12 +0200

    refactoring code

--- js-projects/calculator/script.js
+++ js-projects/calculator/script.js
@@ -2,26 +2,27 @@ window.addEventListener("DOMContentLoaded", () => {
 	const result = document.querySelector(".result"),
 		buttons = document.querySelector(".buttons");
 
+	let a;
+
 	buttons.addEventListener("click", (event) => {
-		// Numbers, dot and zero
-		if (+event.target.innerHTML || event.target.innerHTML == "." || event.target.innerHTML == "0") {
-			result.innerHTML += event.target.innerHTML;
-		}
-		// Clear all
-		if (event.target.innerHTML === "C") {
+		if (event.target.id === "item-0") {
+			// Square root
+			result.innerHTML = Math.sqrt(+result.innerHTML);
+		} else if (event.target.id === "item-1") {
+			// Clear all
 			result.innerHTML = "";
-		}
-		// Backspace
-		if (event.target.id === "item-2") {
+		} else if (event.target.id === "item-2") {
+			// Backspace
 			const resultArr = result.innerHTML.split("");
 			if (resultArr.length > 0) {
 				resultArr.length--;
 			}
 			result.innerHTML = resultArr.join("");
 		}
-		// Square root
-		if (event.target.id === "item-0") {
-			result.innerHTML = Math.sqrt(+result.innerHTML);
+
+		if (+event.target.innerHTML || event.target.id === "item-18" || event.target.id === "item-17") {
+			// Numbers, dot and zero
+			result.innerHTML += event.target.innerHTML;
 		}
 	});
 });
