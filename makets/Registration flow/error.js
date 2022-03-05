cc2c2d0fd95a5ef6215f8093358a682de5cc09e1
Andrey Slynko <qkston22@gmail.com>
2022-03-05 14:48:54 +0200

    FIX: show error message near required inputs

--- makets/Registration flow/error.js
+++ makets/Registration flow/error.js
@@ -1,62 +1,65 @@
 (() => {
-  const requiredInputs = document.querySelectorAll(".required-input");
-  const errorMessages = document.querySelectorAll(".main-form-label-error");
+  window.addEventListener("DOMContentLoaded", () => {
+    const requiredInputs = document.querySelectorAll(".required-input"),
+      errorMessages = document.querySelectorAll(".main-form-label-error"),
+      labelForIncorrectPasswords = document.getElementById("error-passwords"),
+      passwordInputs = document.querySelectorAll(".password-inputs"),
+      checkboxInput = document.getElementById("default-checkbox"),
+      termsText = document.getElementById("agree-terms"),
+      registerBtn = document.getElementById("register-button");
 
-  const labelForIncorrectPasswords = document.getElementById("error-passwords");
-  const passwordInputs = document.querySelectorAll(".password-inputs");
+    // Register button clicked
+    registerBtn.addEventListener("click", e => {
+      e.preventDefault();
 
-  const checkboxInput = document.getElementById("default-checkbox");
-  const termsText = document.getElementById("agree-terms");
+      const isEmpty = Array.from(requiredInputs).some(element => element.value === "");
+      const checkedPasswords = passwordInputs[0].value === passwordInputs[1].value;
 
-  const registerBtn = document.getElementById("register-button");
-
-  // Register button clicked
-  registerBtn.addEventListener("click", e => {
-    e.preventDefault();
-
-    const isEmpty = Array.from(requiredInputs).some(element => element.value === "");
-    const checkedPasswords = passwordInputs[0].value === passwordInputs[1].value;
-
-    requiredInputs.forEach(() => {
-      if (isEmpty) {
-        requiredInputsIsEmpty();
-      } else if (!checkedPasswords) {
-        passwordsCorrect();
-      } else if (!checkboxInput.checked) {
-        checkboxChecked();
-      } else {
-        location.href = "https://qkston.github.io/makets/Registration%20flow/success-register.html";
-      }
+      requiredInputs.forEach(() => {
+        if (isEmpty) {
+          requiredInputsIsEmpty();
+        } else if (!checkedPasswords) {
+          passwordsCorrect();
+        } else if (!checkboxInput.checked) {
+          checkboxChecked();
+        } else {
+          location.href = "https://qkston.github.io/makets/Registration%20flow/success-register.html";
+        }
+      });
     });
-  });
 
-  // Checking of the cumulative fields for emptiness
-  function requiredInputsIsEmpty() {
-    errorMessages.forEach(element => {
-      element.style.display = "inline-block";
+    // Checking of the cumulative fields for emptiness
+    function requiredInputsIsEmpty() {
+      requiredInputs.forEach((input, inputsIndex) => {
+        errorMessages.forEach((element, messagesIndex) => {
+          if (!input.value && inputsIndex == messagesIndex) {
+            element.style.display = "inline-block";
+
+            setTimeout(() => {
+              element.style.display = "none";
+            }, 3000);
+          }
+        });
+      });
+    }
+
+    // Checking of the correct passwords
+    function passwordsCorrect() {
+      errorMessages.forEach(element => {
+        element.style.display = "none";
+      });
+
+      labelForIncorrectPasswords.style.display = "inline-block";
 
       setTimeout(() => {
-        element.style.display = "none";
+        labelForIncorrectPasswords.style.display = "none";
       }, 3000);
-    });
-  }
+    }
 
-  // Checking of the correct passwords
-  function passwordsCorrect() {
-    errorMessages.forEach(element => {
-      element.style.display = "none";
-    });
-
-    labelForIncorrectPasswords.style.display = "inline-block";
-
-    setTimeout(() => {
-      labelForIncorrectPasswords.style.display = "none";
-    }, 3000);
-  }
-
-  // Checking checkbox
-  function checkboxChecked() {
-    termsText.innerHTML = `You must agree to <a href="#">terms and conditins</a> to register.`;
-    termsText.style.color = "red";
-  }
+    // Checking checkbox
+    function checkboxChecked() {
+      termsText.innerHTML = `You must agree to <a href="#">terms and conditions</a> to register.`;
+      termsText.style.color = "red";
+    }
+  });
 })();
