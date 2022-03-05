5629d5b494cd148cd56e252ec12b11e8e193953c
Andrey Slynko <qkston22@gmail.com>
2022-03-05 17:41:16 +0200

    FIX: maked the inputs are required on login page

--- makets/Registration flow/js/login-form.js
+++ makets/Registration flow/js/login-form.js
@@ -1,23 +1,25 @@
 (() => {
   window.addEventListener("DOMContentLoaded", () => {
-    const checkbox = document.getElementById("checkbox");
+    const checkbox = document.getElementById("checkbox"),
+      loginBtn = document.querySelector(".log-in-main-form-lower-button"),
+      companyNameInput = document.querySelector(".company-name"),
+      passwordInput = document.querySelector(".password");
+
     let checked = false;
-    // closeBtn = document.getElementById("close-btn"),
-    // requestBtn = document.getElementById("request-password-change");
 
-    //  // Request password change on forgot password page
-    //  requestBtn.addEventListener("click", e => {
-    //    e.preventDefault();
+    loginBtn.addEventListener("click", e => {
+      e.preventDefault();
 
-    //    location.href = "https://qkston.github.io/makets/Registration%20flow/change-password.html";
-    //  });
-
-    //  // Close button on success register page
-    //  closeBtn.addEventListener("click", e => {
-    //    e.preventDefault();
-
-    //    location.href = "https://qkston.github.io/makets/Registration%20flow/log-in.html";
-    //  });
+      if (!companyNameInput.value) {
+        emptyInput(companyNameInput);
+      }
+      if (!passwordInput.value) {
+        emptyInput(passwordInput);
+      }
+      if (companyNameInput.value && passwordInput.value) {
+        console.log("true");
+      }
+    });
 
     // Checkbox on log-in page
     checkbox.addEventListener("click", () => {
@@ -26,7 +28,7 @@
             background-image: url('img/log-in/checked.png');
             background-position: center;
             background-size: 30px;
-         `;
+            `;
 
         checked = true;
       } else if (checked) {
@@ -34,5 +36,32 @@
         checked = false;
       }
     });
+
+    function emptyInput(input) {
+      input.style.border = "1px solid red";
+      input.style.marginTop = "1px";
+
+      setTimeout(() => {
+        input.style.border = "";
+        input.style.marginTop = "";
+      }, 3000);
+    }
   });
 })();
+
+// closeBtn = document.getElementById("close-btn"),
+// requestBtn = document.getElementById("request-password-change");
+
+//  // Request password change on forgot password page
+//  requestBtn.addEventListener("click", e => {
+//    e.preventDefault();
+
+//    location.href = "https://qkston.github.io/makets/Registration%20flow/change-password.html";
+//  });
+
+//  // Close button on success register page
+//  closeBtn.addEventListener("click", e => {
+//    e.preventDefault();
+
+//    location.href = "https://qkston.github.io/makets/Registration%20flow/log-in.html";
+//  });
