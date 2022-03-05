ff6753b2c05c62569611b715ebdaf26c05da9c1c
Andrey Slynko <qkston22@gmail.com>
2022-03-05 16:35:58 +0200

    FIX: for input for work email added regex

--- makets/Registration flow/error.js
+++ makets/Registration flow/error.js
@@ -12,21 +12,62 @@
     registerBtn.addEventListener("click", e => {
       e.preventDefault();
 
+      let emailInput, emailIndex;
+      requiredInputs.forEach((element, index) => {
+        if (element.name === "work-email") {
+          emailInput = element;
+          emailIndex = index;
+        }
+      });
+
       const isEmpty = Array.from(requiredInputs).some(element => element.value === "");
       const checkedPasswords = passwordInputs[0].value === passwordInputs[1].value;
+      const regexEmail = emailInput.value.match(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/);
 
       requiredInputs.forEach(() => {
         if (isEmpty) {
           requiredInputsIsEmpty();
-        } else if (!checkedPasswords) {
-          passwordsCorrect();
-        } else if (!checkboxInput.checked) {
-          checkboxChecked();
-        } else {
-          location.href = "https://qkston.github.io/makets/Registration%20flow/success-register.html";
+          return;
         }
+
+        if (!regexEmail) {
+          correctEmail(emailIndex);
+          return;
+        }
+
+        if (!checkedPasswords) {
+          passwordsCorrect();
+          return;
+        }
+
+        if (!checkboxInput.checked) {
+          checkboxChecked();
+          return;
+        }
+
+        location.href = "https://qkston.github.io/makets/Registration%20flow/success-register.html";
       });
     });
+
+    function correctEmail(index) {
+      errorMessages.forEach((element, messagesIndex) => {
+        if (index === messagesIndex) {
+          element.style.cssText = `
+            display: inline-block;
+            margin-left: 25px;
+          `;
+          element.textContent = "Correct your email";
+
+          setTimeout(() => {
+            element.style.cssText = `
+               display: none;
+               margin-left: 0px;
+            `;
+            element.textContent = "Please fill out this field";
+          }, 3000);
+        }
+      });
+    }
 
     // Checking of the cumulative fields for emptiness
     function requiredInputsIsEmpty() {
